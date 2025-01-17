b70d81af4a503439b56cbff9c8ba46e5ac2494ea
wangy122 <wangy122@chinatelecom.cn>
2025-01-17 17:21:37 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-python -m mkdocs build -f mkdocs.yml -d site 
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 -m mkdocs build -f mkdocs.yml -d site 
