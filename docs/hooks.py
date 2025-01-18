d32d6102b8bd22d176f5fc84c710a88419766e7e
wangy122 <wangy122@chinatelecom.cn>
2025-01-18 18:26:31 +0800

    fix comment bug

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,5 +1,12 @@
 import os
 
 def on_env(env, config, files):
-    env.globals.update({'environ': os.environ})
+    """在所有页面启用评论"""
+    def enable_comments(url):
+        # 为所有页面启用评论，除了首页
+        if url.endswith('index.html') or url == '':
+            return False
+        return True
+
+    env.filters['enable_comments'] = enable_comments
     return env 
